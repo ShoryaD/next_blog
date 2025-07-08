e96d1c675c7264d32340fbe9fbaa1f2974ef3e3c
Shorya Dubey <shoryadubey7610@gmail.com>
2025-07-08 10:49:41 +0530

    First Commit

---
+++ src/lib/placeholder-data.ts
@@ -0,0 +1,27 @@
+export const categories = [
+  "react",
+  "javascript",
+  "css",
+  "animation",
+  "performance",
+  "database",
+];
+
+export const popularPosts = [
+  {
+    id: 1,
+    title: "Spaces vs. Tabs: The Indentation Debate Continues",
+  },
+  {
+    id: 2,
+    title: "Spaces vs. Tabs: The Indentation Debate Continues",
+  },
+  {
+    id: 3,
+    title: "Spaces vs. Tabs: The Indentation Debate Continues",
+  },
+  {
+    id: 4,
+    title: "The Power of Static Typing in Programming",
+  },
+];
