3b84bf4a91b25f68755a58d09e3147f79bc71aad
Shorya Dubey <shoryadubey7610@gmail.com>
2025-07-09 16:36:15 +0530

    production change

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,7 +8,7 @@ export function cn(...inputs: ClassValue[]) {
 export const fetchUrl =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3000/api"
-    : "https://next-blog-cj.vercel.app/api";
+    : "https://next-blog-sd.vercel.app/api";
 
 type ResponseData = {
   category: string;
